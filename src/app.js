321114509b41fa4c9c0f549109b3aac06a5f768a
isabelochoaco <79860946+isabelochoaco@users.noreply.github.com>
2021-04-07 17:50:41 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -32,10 +32,19 @@ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.dat
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
+function search(city) {
 let apiKey = "8b859973e1ad4c56601cd1d705aacb77";
-let city = "New York";
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
